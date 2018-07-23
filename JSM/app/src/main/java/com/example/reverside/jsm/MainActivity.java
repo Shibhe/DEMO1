72c781c8e5750bb118ec8f6ba87d3039a5193802
Joseph <sibiyajvt@gmail.com>
2018-07-23 14:06:30 +0200

    xdvd
    dsfdsf

--- JSM/app/src/main/java/com/example/reverside/jsm/MainActivity.java
+++ JSM/app/src/main/java/com/example/reverside/jsm/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.reverside.jsm;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -9,12 +11,16 @@ public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         webView = findViewById(R.id.webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
 
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("http://bits-web.co.za/jsm/index.php");
